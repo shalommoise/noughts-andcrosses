70b5209bbde4f329e9c582f7fa966f2fa932a397
ben mckenna <benmckenna2000@outlook.com>
2020-08-20 17:05:37 +0100

    added more functionallity

--- app.md
+++ app.md
@@ -1,10 +1,10 @@
 # Nought and crosses
 
-### 1. Make table with input text.
+### 1. Make table with input text. x
 
-### 2. Button that saves input text (and does not allow it to change once pressed).
+### 2. Input can only take in 'x' or 'o'. 
 
-### 3. Input can only take in 'x' or 'o'.
+### 3. Once value is entered it can't be changed.
 
 ### 4. Input must start with 'x'.
 
@@ -13,6 +13,8 @@
 ### 6. Game over when all tiles have been filled.
 
 ### 7. Can recognise a winner
+
+
 
 #### 3 'x's or 'o's
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,31 +8,38 @@ class Table extends React.Component {
   handleChange = (changeEvent) => {
     const value = changeEvent.target.value;
     const id = changeEvent.target.id;
-
-    if (value !== "x" && value !== "o") {
+    if(document.getElementById('form').childNodes['12']){
+      document.getElementById('form').childNodes['12'].remove()
+    }
+    if (value !== "x" && value !== "o" && value !== '') {
       const warning = document.createElement("p");
       warning.innerText = "Not Allowed";
-      document.getElementById("form").appendChild(warning);
+      document.getElementById("form").appendChild(warning)
     } else
       this.setState((currentState) => {
         currentState.tableState[id] = value;
       });
   };
 
+  fixChange = (changeEvent) => {
+    changeEvent.target.value = changeEvent.target.value.bind(this)
+    
+  }
+
   render = () => {
     return (
       <form id="form" /*onSubmit={this.handleSubmit}*/>
-        <input type="text" id="0" onChange={this.handleChange} />
-        <input type="text" id="1" onChange={this.handleChange} />
-        <input type="text" id="2" onChange={this.handleChange} />
+        <input type="text" id="0" maxLength='1' onChange={this.handleChange && this.fixChange} />
+        <input type="text" id="1" maxLength='1' onChange={this.handleChange} />
+        <input type="text" id="2" maxLength='1' onChange={this.handleChange} />
         <br />
-        <input type="text" id="3" onChange={this.handleChange} />
-        <input type="text" id="4" onChange={this.handleChange} />
-        <input type="text" id="5" onChange={this.handleChange} />
+        <input type="text" id="3" maxLength='1' onChange={this.handleChange} />
+        <input type="text" id="4" maxLength='1' onChange={this.handleChange} />
+        <input type="text" id="5" maxLength='1' onChange={this.handleChange} />
         <br />
-        <input type="text" id="6" onChange={this.handleChange} />
-        <input type="text" id="7" onChange={this.handleChange} />
-        <input type="text" id="8" onChange={this.handleChange} />
+        <input type="text" id="6" maxLength='1' onChange={this.handleChange} />
+        <input type="text" id="7" maxLength='1' onChange={this.handleChange} />
+        <input type="text" id="8" maxLength='1' onChange={this.handleChange} />
         <br />
       </form>
     );
